c61e23833e226bd2b009d46b739e502d2e9fd6fa
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-10-29 15:41:00 +0500

    Task 10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿int secondDigit(int x)
+{
+    int digit = x / 10;
+    int secondD = digit % 10;
+    return secondD;
+}
+
+int num = new Random().Next(100, 1000);
+System.Console.WriteLine("The number is: " + num);
+System.Console.WriteLine("The second digit of the number is: " + secondDigit(num));
