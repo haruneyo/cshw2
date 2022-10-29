8a944d83ea697b1e571d6e023d9f468a962d0b7c
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-10-29 16:04:23 +0500

    Task 15 done

---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Please enter the number of the day of the week:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 1 || num > 7) System.Console.WriteLine("This day of the week does not exist");
+else System.Console.WriteLine("Is it the weekend? --> " + weekendCheck(num));
+
+bool weekendCheck(int num)
+{
+    if (num == 6 || num == 7) return true;
+    else return false;
+}
