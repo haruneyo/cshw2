9930354dd945d57d8dd76bdbc584ba609b385f7d
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-10-29 15:53:41 +0500

    Task 13 done

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter the number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int findThirdDigit(int input)
+{
+    int newNum = input / 100;
+    int thirdD = newNum % 10;
+    return thirdD;
+}
+
+if (num < 100 && num > -100) System.Console.WriteLine("The number is less than three digits");
+else System.Console.WriteLine(findThirdDigit(num));
